d9bf251700de6af6bb5289e180e6d2d01f115369
sarfoodhouse <sarfoodhouse@gmail.com>
2026-01-25 12:41:32 +0530

    changed the price of the iteams
    changed the price of the iteams

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -138,7 +138,7 @@ export const products: Product[] = [
 { id: "bw1", name: "Water", price: 20, category: "Water" },
 
   //Biryani
-  { id: "by1", name: "Hyd chicken dumbiryani", price: 20, category: "Biryani" },
-  { id: "by2", name: "Hyd Kuska Rice", price: 20, category: "Biryani" },
+  { id: "by1", name: "Hyd chicken dumbiryani", price: 150, category: "Biryani" },
+  { id: "by2", name: "Hyd Kuska Rice", price: 80, category: "Biryani" },
 
 ];
