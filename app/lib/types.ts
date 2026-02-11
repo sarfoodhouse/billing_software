6643d6418ca24a8b7add247cc0c340dafa949cf5
sarfoodhouse <sarfoodhouse@gmail.com>
2026-02-11 19:39:57 +0530

    added grill in type

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -1,5 +1,5 @@
 // Add this to your existing types.ts
-export type Category = "Shawarma" | "Puri"| "Pav Bhaji" | "Fries & Sandwich" | "Chaats" | "Juice" | "Ice Cream" | "Vada Pav" | "Water" | "Biryani" ;
+export type Category = "Shawarma" | "Puri"| "Pav Bhaji" | "Fries & Sandwich" | "Chaats" | "Juice" | "Ice Cream" | "Vada Pav" | "Water" | "Biryani" | "Grill Chicken" ;
 
 export interface Product {
   id: string;
